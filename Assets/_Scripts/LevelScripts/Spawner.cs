44cf8451d2500351fa43fda91e82499e4db00758
Endi Haxhi <endi24haxhi@hotmail.com>
2022-08-01 21:12:11 +0200

    Implemented the correct goal triggers, as well as ball spawn, goal spawns. Need to make this repeat though and add ui, so that when a goal is scored, I can restart it.

--- Assets/_Scripts/LevelScripts/Spawner.cs
+++ Assets/_Scripts/LevelScripts/Spawner.cs
@@ -8,6 +8,9 @@ public class Spawner : MonoBehaviour
     public Transform Player1SpawnPosition;
     public Transform Player2SpawnPosition;
 
+    public float minX1, minY1, maxX1, maxY1;
+    public float minX2, minY2, maxX2, maxY2;
+
     private void Start()
     {
         if (PhotonNetwork.IsConnected)
@@ -15,6 +18,9 @@ public class Spawner : MonoBehaviour
            if (PhotonNetwork.IsMasterClient)
             {
                 SpawnPlayer1();
+                SpawnBall();
+                SpawnGoal1();
+                SpawnGoal2();
             }
             if (!PhotonNetwork.IsMasterClient)
             {
@@ -30,5 +36,22 @@ public class Spawner : MonoBehaviour
     public void SpawnPlayer2()
     {
         PhotonNetwork.Instantiate("Player2", Player2SpawnPosition.position, Quaternion.identity);
+    }
+    public void SpawnBall()
+    {
+        PhotonNetwork.Instantiate("Ball", new Vector3(0, 0.85f, 0), Quaternion.identity);
+    }
+    public void SpawnGoal1()
+    {
+        //possibly assign a color
+        Vector3 randomPosition = new Vector3(Random.Range(minX1, maxX1), 0.7f, Random.Range(minY1, maxY1));
+        PhotonNetwork.Instantiate("Goal1", randomPosition, Quaternion.identity);
+    }
+
+    public void SpawnGoal2()
+    {
+        //possibly assign a color
+        Vector3 randomPosition = new Vector3(Random.Range(minX2, maxX2), 0.7f, Random.Range(minY2, maxY2));
+        PhotonNetwork.Instantiate("Goal2", randomPosition, Quaternion.identity);
     }
 }
