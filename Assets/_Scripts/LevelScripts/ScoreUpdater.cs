86a5cf91f319d2a81e41115d542ffdc23b65a2b9
Endi Haxhi <endi24haxhi@hotmail.com>
2022-08-02 02:03:20 +0200

    When a player scores, I save the score in the Game Controller, and then reload the level after a delay. Much cleaner, works wonders.

--- Assets/_Scripts/LevelScripts/ScoreUpdater.cs
+++ Assets/_Scripts/LevelScripts/ScoreUpdater.cs
@@ -2,41 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
+using TMPro;
 
 public class ScoreUpdater : MonoBehaviour
 {
     public Slider Player1ScoreSlider;
     public Slider Player2ScoreSlider;
 
-    private int p1Score = 0;
-    private int p2Score = 0;
+    public GameObject PlayerScoredPanel;
+    public TextMeshProUGUI PlayerScoredText;
 
     private GameOver gameOver;
-
+    private PhotonView view;
     private void Start()
     {
+        view = GetComponent<PhotonView>();
+        Player1ScoreSlider.value = GameController.Instance.Player1Choice.PlayerPoints;
+        Player2ScoreSlider.value = GameController.Instance.Player2Choice.PlayerPoints;
         gameOver = FindObjectOfType<GameOver>();
     }
     public void UpdateP1Score()
     {
-        p1Score++;
-        GameController.Instance.Player1Choice.PlayerPoints = p1Score;
-        Player1ScoreSlider.value = (int)p1Score;
-        if (p1Score == 3 || GameController.Instance.Player1Choice.PlayerPoints == 3)
+        //p1Score++;
+        GameController.Instance.Player1Choice.PlayerPoints++; // = p1Score;
+        Player1ScoreSlider.value = (int)GameController.Instance.Player1Choice.PlayerPoints;
+        if (GameController.Instance.Player1Choice.PlayerPoints == 3)
         {
             gameOver.ShowGameOverScreen();
+        }
+        else
+        {
+            pointScored(true);
         }
     }
 
     public void UpdateP2Score()
     {
-        p2Score++;
-        GameController.Instance.Player2Choice.PlayerPoints = p2Score;
-        Player2ScoreSlider.value = (int)p2Score;
+        //p2Score++;
+        GameController.Instance.Player2Choice.PlayerPoints++; // = p2Score;
+        Player2ScoreSlider.value = (int)GameController.Instance.Player2Choice.PlayerPoints;
 
-        if (p2Score == 3 || GameController.Instance.Player2Choice.PlayerPoints == 3)
+        if (GameController.Instance.Player2Choice.PlayerPoints == 3)
         {
             gameOver.ShowGameOverScreen();
         }
+        else
+        {
+            pointScored(false);
+        }
+    }
+
+    private void pointScored(bool wasHost)
+    {
+        PlayerScoredPanel.SetActive(true);
+        if (wasHost)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {                
+                PlayerScoredText.text = "You Scored! Yay!";
+            }
+            else if (!PhotonNetwork.IsMasterClient)
+            {
+                PlayerScoredText.text = "Enemy Scored! Defend Better!";
+            }
+        }
+        else if (!wasHost)
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                PlayerScoredText.text = "You Scored! Yay!";
+            }
+            else if (PhotonNetwork.IsMasterClient)
+            {
+                PlayerScoredText.text = "Enemy Scored! Defend Better!";
+            }
+        }
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Invoke("LoadGameAgain", 1.5f);
+        }
+    }
+    void LoadGameAgain()
+    {
+        view.RPC("LoadGameAgainRPC", RpcTarget.All);
+    }
+
+    [PunRPC]
+    void LoadGameAgainRPC()
+    {
+        PhotonNetwork.LoadLevel("InGame3D");
     }
 }
